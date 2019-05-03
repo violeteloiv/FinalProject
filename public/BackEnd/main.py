fb7963a77df61caa6bb576ef45a3810463adddc0
= <theepicpenguin121@gmail.com>
2019-05-03 11:02:47 -0700

    Create SpreadSheet Class
    Create A SpreadSheet Object For Easier Access.

--- public/BackEnd/main.py
+++ public/BackEnd/main.py
@@ -7,8 +7,34 @@ scope = ['https://spreadsheets.google.com/feeds',
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
-sheet = client.open("Data").sheet1
-
 pp = pprint.PrettyPrinter()
-data = sheet.get_all_records()
-pp.pprint(data)
+
+class SpreadSheet:
+
+    def __init__(self):
+        self.currentSheet = client.open("Data").sheet1
+        self.data = None
+
+    def setup(self):
+        self.currentSheet.clear()
+        self.currentSheet.update_cell(1, 1, "Names:")
+        self.currentSheet.update_cell(1, 2, "Current Level:")
+        self.currentSheet.update_cell(1, 3, "Health:")
+
+        self.data = self.currentSheet.get_all_records()
+
+    def clear_data(self):
+        self.currentSheet.clear()
+
+    def print_data(self):
+        if self.data == None:
+            print("Currently No Data")
+        else:
+            pp.pprint(self.data)
+
+s_data = SpreadSheet()
+s_data.clear_data()
+
+s_data.setup()
+
+s_data.print_data()
