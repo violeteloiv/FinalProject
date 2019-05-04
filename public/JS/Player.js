6665b305515c91f76250016c089326caa3517850
TheEpicPenguin13 <TheEpicPenguin13@users.noreply.github.com>
2019-05-03 18:01:59 -0700

    Added Key Movement
    Added Key Movement To The Player Class Since It Would Take Up More Space In The Main.js Script If I Put It There.

--- public/JS/Player.js
+++ public/JS/Player.js
@@ -6,10 +6,13 @@ export default class Player
 
         this.pos = this.s.createVector(10, 10);
         this.size = this.s.createVector(50, 50);
+
+        this.speed = 4;
     }
 
     update()
     {
+        this.move();
         this.show();
     }
 
@@ -18,5 +21,25 @@ export default class Player
         this.s.fill(255);
         this.s.noStroke();
         this.s.rect(this.pos.x, this.pos.y, this.size.x, this.size.y);
+    }
+
+    move()
+    {
+        if (this.s.keyIsDown(87)) // W Key
+        {
+            this.pos.y -= this.speed;
+        }
+        if (this.s.keyIsDown(65)) // A Key
+        {
+            this.pos.x -= this.speed;
+        }
+        if (this.s.keyIsDown(83)) // S Key
+        {
+            this.pos.y += this.speed;
+        }
+        if (this.s.keyIsDown(68)) // D Key
+        {
+            this.pos.x += this.speed;
+        }
     }
 }
