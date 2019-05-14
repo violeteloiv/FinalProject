0fe2615f05f9e034dd31f7825c55e63f3b2b8732
= <theepicpenguin121@gmail.com>
2019-05-14 11:31:41 -0700

    Jumping / Added To Game

--- public/JS/Scenes/GameScene.js
+++ public/JS/Scenes/GameScene.js
@@ -19,9 +19,9 @@ export default class Game extends Scene
 
     setupGame()
     {
-        player = new Player(this.s);
+        player = new Player(this.s, this);
         this.pushToUpdate(new Rectangle(this.s, 0, 0, this.s.width, this.s.height, "#000000", undefined));
-        this.pushToUpdate(new Platform(this.s, 0, 400, this.s.width, this.s.height - 400));
+        this.pushToUpdate(new Platform(this.s, "floor", 0, 400, this.s.width, this.s.height - 400));
 
         this.pushToUpdate(player);
     }
