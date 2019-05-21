cdbebcebadd3cd84bc329bc406d54042b9f6071b
= <theepicpenguin121@gmail.com>
2019-05-21 11:49:35 -0700

    CLASSWORK

--- public/JS/Constants.js
+++ public/JS/Constants.js
@@ -1,4 +1,5 @@
 const GRAVITY = 0.38;
 const MAX_HEALTH = 100;
 const MAX_DIST_TILL_ATK_1 = 200;
-const NUM_ENEMIES_1 = 1;
+const NUM_ENEMIES_1 = 2;
+const FRAMERATE = 60;
--- public/JS/Physics/Projectile.js
+++ public/JS/Physics/Projectile.js
@@ -6,6 +6,7 @@ export default class Projectile
     {
         this.s = s;
         this.name = name;
+        this.player = player;
         this.distFromFire = 0;
         this.pos = this.s.createVector(player.hand.pos.x, player.hand.pos.y);
         this.circle = new Circle(s, this.pos.x, this.pos.y, 5, 5, "#ffff00", "#ffff00");
@@ -18,8 +19,8 @@ export default class Projectile
 
         if (this.fired)
         {
-            let prevPos = this.s.createVector(this.pos.x)
+            let prevPos = this.s.createVector(this.pos.x, this.pos.y);
+            this.circle.fc = "red";
         }
-
     }
 }
